75e37695d59aa1dd799cbc8fd6276e7ed07d9997
Sergei Udris <sergei.udris@gmail.com>
2021-07-13 17:15:20 +0300

    Revert "Revert "remove authors, license, attributions - repo is code only""
    This reverts commit 5c025c4fd1279b151b8791b52566caba67a3a796.

--- examples/laptop/WebGLExport.py
+++ examples/laptop/WebGLExport.py
@@ -7,29 +7,6 @@ Group: 'Export'
 Tooltip: 'WebGL JavaScript'
 """
 
-# --------------------------------------------------------------------------
-# ***** BEGIN GPL LICENSE BLOCK *****
-#
-# Copyright (C) 2010 Dennis Ippel
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software Foundation,
-# Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-#
-# ***** END GPL LICENCE BLOCK *****
-# --------------------------------------------------------------------------
-
-__author__ = "Dennis Ippel"
 __url__ = ("http://www.rozengain.com")
 __version__ = "0.2"
 
